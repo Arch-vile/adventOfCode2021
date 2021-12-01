032b5ace0f7dda2ff360a32fc2b49cabae74c78d
Arch-vile <mikko.ravimo@gmail.com>
2021-12-01 20:46:09 +0200

    Day 1 part 2 complete

--- src/main/kotlin/day1/DepthMonitor.kt
+++ src/main/kotlin/day1/DepthMonitor.kt
@@ -3,15 +3,17 @@ package day1
 import utils.read
 
 fun main(args: Array<String>) {
-    val count = read("./src/main/resources/day1Input.txt")
-        .asSequence()
-        .map { it.toInt() }
-        .windowed(2,1)
-        .map { it[1] - it[0] }
-        .filter { it > 0 }
-        .count()
+    val data = read("./src/main/resources/day1Input.txt").asSequence().map { it.toInt() }
+    println("part1: ${countDepth(data)}")
 
-    println(count);
+    val windowedData = data.windowed(3,1).map { it.sum() }
+    println("part2: ${countDepth(windowedData)}")
 }
+
+private fun countDepth(data: Sequence<Int>) = data
+    .windowed(2, 1)
+    .map { it[1] - it[0] }
+    .filter { it > 0 }
+    .count()
 
 
