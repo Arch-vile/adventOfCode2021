d5aef6fa0195173cea62961661a36dc7fdef5fd2
Arch-vile <mikko.ravimo@gmail.com>
2021-12-03 23:49:57 +0200

    Naive loop to find the oxygen

--- src/main/kotlin/day3/Diagnostics.kt
+++ src/main/kotlin/day3/Diagnostics.kt
@@ -5,21 +5,35 @@ import utils.rotateCW
 
 fun main() {
 
-    val data = read("./src/main/resources/day3Input.txt")
+    val data = read("./src/main/resources/day3SampleInput.txt")
         .map{ it.toCharArray().toList()}
         .map { it.map { it.toString().toLong() } }
 
     var mostCommonBits = rotateCW(data)
         .map { it.map { value -> if (value == 0L) -1 else 1 } }
         .map { it.sum() }
-        .map { if (it < 0) 0 else 1 }
+        .map { if (it < 0) 0L else 1L }
 
     val gamma = mostCommonBits.joinToString("").toLong(2)
 
     val epsilon = mostCommonBits
-        .map { if (it == 1) 0 else 1 }
+        .map { if (it == 1L) 0 else 1 }
         .joinToString("")
         .toLong(2)
 
+    var filteroidaan = data
+
+    for (i in mostCommonBits.indices) {
+        val eka = mostCommonBits[i]
+        filteroidaan = data.filter { it[i] != eka }
+
+        if(filteroidaan.size == 1) {
+           break
+        }
+    }
+
     println(gamma*epsilon)
+    println(filteroidaan.first())
 }
+
+
