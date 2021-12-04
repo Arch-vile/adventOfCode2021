9106b4378219045b7579cbfe70c5d43493931a94
Arch-vile <mikko.ravimo@gmail.com>
2021-12-04 16:16:25 +0200

    Use binary helper on part 1

--- src/main/kotlin/day3/Diagnostics.kt
+++ src/main/kotlin/day3/Diagnostics.kt
@@ -1,39 +1,50 @@
 package day3
 
+import utils.Binary
 import utils.read
-import utils.rotateCW
 
 fun main() {
+    val data = read("./src/main/resources/day3Input.txt")
+        // Input binaries are least significant bits first, so need to reverse
+        .map { it.reversed() }
+        .map { Binary.from(it) }
 
-    val data = read("./src/main/resources/day3SampleInput.txt")
-        .map{ it.toCharArray().toList()}
-        .map { it.map { it.toString().toLong() } }
+    var mostCommonBits = mostCommonBits(data)
+    val gamma = mostCommonBits.asLong()
+    val epsilon = mostCommonBits.invert().asLong()
 
-    var mostCommonBits = rotateCW(data)
-        .map { it.map { value -> if (value == 0L) -1 else 1 } }
-        .map { it.sum() }
-        .map { if (it < 0) 0L else 1L }
-
-    val gamma = mostCommonBits.joinToString("").toLong(2)
-
-    val epsilon = mostCommonBits
-        .map { if (it == 1L) 0 else 1 }
-        .joinToString("")
-        .toLong(2)
-
-    var filteroidaan = data
-
-    for (i in mostCommonBits.indices) {
-        val eka = mostCommonBits[i]
-        filteroidaan = data.filter { it[i] != eka }
-
-        if(filteroidaan.size == 1) {
-           break
-        }
-    }
-
-    println(gamma*epsilon)
-    println(filteroidaan.first())
+    println(gamma * epsilon)
 }
+
+fun solve(data: List<List<Long>>): List<Long> {
+
+    if (data.size == 1) {
+        return data.first()
+    }
+    else {
+//        val commonBits = mostCommonBits(data)
+
+        return data[0];
+//        val newData =
+//            filter data(1) == mostCommonBit
+
+    }
+}
+
+
+private fun mostCommonBits(data: List<Binary>): Binary {
+    // All rows have 12 bits
+    val mostCommon = (0 until 12)
+        // Gives us the vertical bit rows
+        .map { index -> data.map { it.bit(index) } }
+        // Lets change 0 to -1
+        .map { it.map { bit -> if (bit == 0) -1 else 1 } }
+        // And finally if we take sum, we know did we have more 1's than 0's
+        .map { if (it.sum() < 0) 0 else 1 }
+
+    return Binary.from(mostCommon)
+}
+
+
 
 
