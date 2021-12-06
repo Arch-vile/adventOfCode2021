a3104acee66cf7e7ec67993ccfecaff98a10052c
Arch-vile <mikko.ravimo@gmail.com>
2021-12-06 11:40:11 +0200

    Day 5 part 1 started

---
+++ src/main/kotlin/day5/HydroVents.kt
@@ -0,0 +1,30 @@
+package day5
+
+import utils.Point
+import utils.Matrix
+import utils.read
+
+data class Line(val start: Point, val end: Point)
+
+fun main() {
+    val lines = read("./src/main/resources/day5SampleInput.txt")
+        .map { it.split(" ") }
+        .map { line ->
+            val firsCoord = line[0].split(",") // ['5','6']
+            val secondCoord = line[2].split(",")
+            Line(
+                Point(firsCoord[0].toLong(), firsCoord[1].toLong()),
+                Point(secondCoord[0].toLong(), secondCoord[1].toLong()),
+                )
+        }
+
+    val allPointsInLines = lines.flatMap { pointsInLine(it) }
+    val seaFloor = Matrix(10,10){ x,y -> 0 }
+    allPointsInLines.forEach { point ->
+        seaFloor.replace(point.x, point.y) { entry -> entry.value + 1 }
+    }
+}
+
+fun pointsInLine(line: Line): List<Point> {
+   TODO()
+}
--- src/main/kotlin/utils/Matrix.kt
+++ src/main/kotlin/utils/Matrix.kt
@@ -12,6 +12,8 @@ class Matrix<T>(input: List<List<T>>) {
         }
     }
 
+    constructor(width: Long, height: Long, init: (Int,Int) -> T) : this(initialize(width, height, init))
+
     fun find(value: T): Entry<T>? {
         for (y in 0 until data.size) {
             for (x in 0 until data.first().size) {
@@ -23,6 +25,10 @@ class Matrix<T>(input: List<List<T>>) {
 
     fun replace(found: Entry<T>, value: T) {
         data[found.y][found.x] = value
+    }
+
+    fun replace(found: Long, value: Long, provider: (Entry<T>) -> T) {
+        TODO("Not yet implemented")
     }
 
     // Rows for which the matcher matches for each element in row
@@ -43,13 +49,13 @@ class Matrix<T>(input: List<List<T>>) {
 
     // Columns for which the matcher matches for each element in column
     fun findColsByValues(matcher: (T) -> Boolean): List<List<T>> {
-       return rotateCW().findRowsByValues(matcher)
+        return rotateCW().findRowsByValues(matcher)
     }
 
     fun rotateCW(): Matrix<T> {
-        val foo = IntRange(0,data[0].size-1)
-            .map {x ->
-                ((data.size-1) downTo 0).map { y ->
+        val foo = IntRange(0, data[0].size - 1)
+            .map { x ->
+                ((data.size - 1) downTo 0).map { y ->
                     data[y][x]
                 }
             }
@@ -58,6 +64,13 @@ class Matrix<T>(input: List<List<T>>) {
 
     override fun toString(): String {
         return data.joinToString("\n") { it.toString() };
+    }
+
+
+    companion object {
+        private fun <T> initialize(width: Long, height: Long, init: (x: Int, y: Int) -> T): List<List<T>> {
+            TODO("Not yet implemented")
+        }
     }
 
 
