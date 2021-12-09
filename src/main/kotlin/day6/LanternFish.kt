407cf1f6b4d6b78f64fec4050e698e87ed259fbe
Arch-vile <mikko.ravimo@gmail.com>
2021-12-09 09:12:03 +0200

    Some cleanup for day 6

--- src/main/kotlin/day6/LanternFish.kt
+++ src/main/kotlin/day6/LanternFish.kt
@@ -13,70 +13,25 @@ fun solve(): List<Long> {
     return listOf(countFish(startingFishes, 80), countFish(startingFishes, 256))
 }
 
-private fun countFish(startingFishes: List<Int>, days: Int): Long {
+private fun countFish(startingFishes: List<Int>, totalDays: Int): Long {
     val cache = mutableMapOf<String, Long>()
 
-    // ticker only matters for the initial ones
-    fun countDescendants(totalDays: Int, dayNow: Int, ticker: Int): Long {
+    fun countDescendants(dayNow: Int, ticker: Int): Long {
+        if (dayNow > totalDays) return 0
+        if (cache.contains("$dayNow,$ticker")) return cache["$dayNow,$ticker"]!!
 
-        if (dayNow > totalDays) {
-            return 0;
-        }
+        val firstSpawnOn = dayNow + ticker
+        val spawnOn = (firstSpawnOn..totalDays step 7)
 
-        if (cache.contains("$dayNow,$ticker")) {
-            return cache["$dayNow,$ticker"]!!
-        }
-
-        val firstReroduceOn = dayNow + ticker
-        val daysOnWhichIReproduce =
-            listOf(firstReroduceOn).plus(
-                (firstReroduceOn + 7..totalDays step 7)
-            )
-
-        // Myself
+        // Count myself
         val result = 1 +
-                // all my children and their descendants
-                daysOnWhichIReproduce.sumOf { dayOfReproduce ->
-                    countDescendants(totalDays, dayOfReproduce, 9)
-                }
+                // add all my children and their descendants
+                spawnOn.sumOf { day -> countDescendants(day, 9) }
 
-        cache.put("$dayNow,$ticker", result)
+        cache["$dayNow,$ticker"] = result
 
         return result
     }
 
-    return startingFishes.sumOf {
-        countDescendants(days, 1, it)
-    }
-}
-
-
-
-private fun reproduce(
-    days: Int,
-    fish: List<Int>
-): List<Int> {
-    var fish1 = fish
-    (1..days)
-        .forEach { day ->
-
-            if (day % 10 == 0)
-                println(day)
-
-            fish1 = fish1.flatMap {
-                val newFish = mutableListOf<Int>()
-                if (it == 0) {
-                    newFish.add(8)
-                }
-                var newValue = if (it == 0) {
-                    6
-                }
-                else {
-                    it - 1
-                }
-                newFish.add(newValue)
-                newFish
-            }
-        }
-    return fish1
+    return startingFishes.sumOf { countDescendants( 1, it) }
 }
